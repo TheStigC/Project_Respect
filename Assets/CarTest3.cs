17bbc6119521effac497861001fa2b1bdc9d394f
Stig-PC <stigc@live.dk>
2018-10-12 15:39:11 +0200

    Test af møgbiler, øv. Og joystick inputs tilføjet

---
+++ Assets/CarTest3.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarTest3 : MonoBehaviour
+{
+    public Vector2 move;
+    public Vector2 velocity; // in metres per second
+    public float maxSpeed = 5.0f; // in metres per second
+    public float acceleration = 5.0f; // in metres/second/second
+    public float brake = 5.0f; // in metres/second/second
+    public float turnSpeed = 30.0f; // in degrees/second
+    private float speed = 0.0f;    // in metres/second
+    Rigidbody rb;
+
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // the horizontal axis controls the turn
+        float turn = Input.GetAxis("Horizontal");
+
+
+
+        // the vertical axis controls acceleration fwd/back
+        float forwards = Input.GetAxis("Vertical");
+        if (forwards > 0)
+        {
+            // accelerate forwards
+            speed = speed + acceleration * Time.deltaTime;
+
+            if (speed >= 1)
+            {
+                // turn the car
+                transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
+            }
+
+        }
+        else if (forwards < 0)
+        {
+            // accelerate backwards
+            speed = speed - acceleration * Time.deltaTime;
+
+            if (speed <= 1)
+            {
+                // turn the car
+                transform.Rotate(0, turn * -turnSpeed * Time.deltaTime, 0);
+            }
+        }
+        else
+        {
+            // braking
+            if (speed > 0)
+            {
+                speed = speed - brake * Time.deltaTime;
+            }
+            else
+            {
+                speed = speed + brake * Time.deltaTime;
+            }
+        }
+
+        // clamp the speed
+        speed = Mathf.Clamp(speed, -maxSpeed, maxSpeed);
+        // compute a vector in the up direction of length speed
+        Vector2 velocity = Vector3.right * speed;
+        // move the object
+        rb.velocity = (velocity * Time.deltaTime);
+    }
+}
---
+++ Assets/TestPlayer.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class TestPlayer : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float hInput = 0.0f;
+    public float vInput = 0.0f;
+
+    public float speed = 200.0f;
+    public float turnRate = 3.0f;
+
+    Quaternion targetRotation;
+
+    private Transform myTransform;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        targetRotation = Quaternion.identity;
+
+        myTransform = transform;
+    }
+
+
+    void Update()
+    {
+        hInput = CrossPlatformInputManager.GetAxis("Horizontal");
+        vInput = CrossPlatformInputManager.GetAxis("Vertical");
+    }
+
+
+    void FixedUpdate()
+    {
+        Rotation(hInput, vInput);
+        Movement(hInput, vInput);
+    }
+
+    void Rotation(float horizontal, float vertical)
+    {
+        float angle = Mathf.Atan2(hInput, vInput) * Mathf.Rad2Deg;
+        float step = turnRate * Time.deltaTime;
+
+        if (horizontal != 0 || vertical != 0)
+        {
+            targetRotation.eulerAngles = new Vector3(0, angle, 0);
+
+            myTransform.rotation = Quaternion.RotateTowards(myTransform.rotation, targetRotation, step);
+        }
+    }
+
+    void Movement(float horizontal, float vertical)
+    {
+        if (horizontal != 0.0f || vertical != 0.0f)
+        {
+            rb.AddForce(myTransform.forward * speed * Time.fixedDeltaTime);
+        }
+    }
+}
