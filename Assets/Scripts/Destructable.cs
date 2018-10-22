7cea24889cf8693e9fa7e395bf7a884017e7241b
Stig-PC <stigc@live.dk>
2018-10-22 09:11:34 +0200

    Added healthbars, mangled with camerashake and tested pixel effect

--- Assets/Scripts/Destructable.cs
+++ Assets/Scripts/Destructable.cs
@@ -1,21 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Destructable : MonoBehaviour
 {
 
-    public float health = 50f;
+    public float startHealth = 50f;
+    private float health;
     private bool isDead;
+    public GameObject deathParticle;
+
+    [Header("Unity Stuff")]
+    public GameObject healthBarObject;
+    public Image healthBarFill;
+
 
     void Start()
     {
+        health = startHealth;
+        if (healthBarObject != null)
+        {
+            healthBarObject.SetActive(false);
+        }
     }
 
 
     public void TakeDamage(float amount)
     {
         health -= amount;
+        if (healthBarObject != null)
+        {
+            StartCoroutine(ShowHealtBar());
+            healthBarFill.fillAmount = health / startHealth;
+        }
+
         if (health <= 0 && !isDead)
         {
             Die();
@@ -27,6 +46,21 @@ public class Destructable : MonoBehaviour
     void Die()
     {
         isDead = true;
+        SpawnDeathParticle();
         Destroy(gameObject);
+    }
+
+    void SpawnDeathParticle()
+    {
+        GameObject particleClone;
+        particleClone = Instantiate(deathParticle, this.transform.position, this.transform.rotation) as GameObject;
+        Destroy(particleClone, 3f);
+    }
+
+    IEnumerator ShowHealtBar()
+    {
+        healthBarObject.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        healthBarObject.SetActive(false);
     }
 }
