0cbfa1769a87f072a700510821f762f054f21f60
Stig-PC <stigc@live.dk>
2018-10-25 16:18:49 +0200

    Fixed Merge Hell

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -1,15 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class EnemyBehaviour : MonoBehaviour
 {
-
-
     public float speed, stoppingDistance, retreatDistance, attackSpeed, rotationSpeed;
     private float timeBetweenShots;
-    public Transform player;
+    public GameObject player;
     public GameObject projectile, firePoint;
+    NavMeshAgent agent;
 
 
 
@@ -17,39 +18,28 @@ public class EnemyBehaviour : MonoBehaviour
 
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-
+        player = GameObject.FindGameObjectWithTag("Player");
+        agent = GetComponent<NavMeshAgent>();
         timeBetweenShots = attackSpeed;
     }
 
 
     void Update()
     {
-        //FIND PLAYER ROTATION AND ROTATE TOWARDS HIM
-        Vector3 targetDir = player.position - transform.position;
-        float step = rotationSpeed * Time.deltaTime;
-        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0f);
-
-
-
-        //CHASE A PLAYER
-        if (Vector3.Distance(transform.position, player.position) >= stoppingDistance)
+        if (player != null)
         {
-            transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
-            transform.rotation = Quaternion.LookRotation(newDir);
+            FollowTarget(player.GetComponent<Target>());
         }
-        //STAY IDLE
-        else if (Vector3.Distance(transform.position, player.position) <= stoppingDistance && Vector3.Distance(transform.position, player.position) >= retreatDistance)
-        {
-            transform.position = this.transform.position;
-            transform.rotation = Quaternion.LookRotation(newDir);
-        }
-        //RETREAT
-        else if (Vector3.Distance(transform.position, player.position) <= retreatDistance)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
-            transform.rotation = Quaternion.LookRotation(newDir);
-        }
+        /*  //FIND PLAYER ROTATION AND ROTATE TOWARDS HIM
+          Vector3 targetDir = player.position - transform.position;
+          float step = rotationSpeed * Time.deltaTime;
+          Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0f);
+          //RETREAT
+          if (Vector3.Distance(transform.position, player.position) <= retreatDistance)
+          {
+              transform.position = Vector3.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+              transform.rotation = Quaternion.LookRotation(newDir);
+          }*/
 
 
         //SHOOT
@@ -63,5 +53,27 @@ public class EnemyBehaviour : MonoBehaviour
             timeBetweenShots -= Time.deltaTime;
         }
 
+
+    }
+    void SetFocus()
+    {
+        FollowTarget(player.GetComponent<Target>());
+        player.GetComponent<Target>().OnFococused(transform);
+    }
+
+
+    public void FollowTarget(Target newTarget)
+    {
+        agent.SetDestination(player.transform.position);
+        FaceTarget();
+        agent.stoppingDistance = newTarget.radius * .8f;
+        agent.updateRotation = false;
+        // player.transform = newTarget.playerTransform;
+    }
+    void FaceTarget()
+    {
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
     }
 }
