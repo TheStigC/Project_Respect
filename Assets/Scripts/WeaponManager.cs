8e7bb764fbfb704e9fabf3a5cb47c9409e6134d3
Stig-PC <stigc@live.dk>
2018-10-25 15:08:45 +0200

    Weaponswitch updated, added icons for active weapon
    Weaponswitch updated, added icons for active weapon

--- Assets/Scripts/WeaponManager.cs
+++ Assets/Scripts/WeaponManager.cs
@@ -7,11 +7,15 @@ using UnityStandardAssets.CrossPlatformInput;
 public class WeaponManager : MonoBehaviour
 {
 
-    public GameObject projectilePrefabHand, projectilePrefabPistol, projectilePrefabShotgun;
-    public Transform handFirePoint, firepointPistol, firepointShotgun;
-    public GameObject handWeapon, pistol, shotgun;
+    public GameObject projectilePrefabHand, projectilePrefabBaseballBat, projectilePrefabPistol, projectilePrefabShotgun, projectilePrefabBazooka;
+    public Transform firepointHand, firepointBaseballBat, firepointPistol, firepointShotgun;
+    public GameObject handWeapon, baseballBat, pistol, shotgun, bazooka;
     public int previousWeapon, selectedWeapon = 0;
     private CameraShake camShake;
+    public Image iconContainer;
+    public bool handsUnlocked = true, baseballBatUnlocked = false, pistolUnlocked = false, shotgunUnlocked = false, bazookaUnlocked = false;
+    public List<Sprite> iconsList;
+
 
 
     void Start()
@@ -25,47 +29,35 @@ public class WeaponManager : MonoBehaviour
     {
         //SELECTING WEAPON
         previousWeapon = selectedWeapon;
+
+
         if (CrossPlatformInputManager.GetAxis("Mouse ScrollWheel") > 0f || CrossPlatformInputManager.GetButtonDown("SwitchWeaponUp"))
         {
-            if (selectedWeapon >= transform.childCount - 1)
-            {
-                selectedWeapon = 0;
-            }
-            else
-            {
-                selectedWeapon++;
-            }
+            ScrollForth();
         }
         if (CrossPlatformInputManager.GetAxis("Mouse ScrollWheel") < 0f || CrossPlatformInputManager.GetButtonDown("SwitchWeaponDown"))
         {
-            Debug.Log("jeg ruller ned");
-            if (selectedWeapon <= 0)
-            {
-                selectedWeapon = transform.childCount - 1;
-            }
-            else
-            {
-                selectedWeapon--;
-            }
+            ScrollBack();
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+
+        if (Input.GetKeyDown(KeyCode.Alpha1) && handsUnlocked)
         {
             SwitchToWeapon(0);
         }
-        if (Input.GetKeyDown(KeyCode.Alpha2) && transform.childCount >= 2)
+        if (Input.GetKeyDown(KeyCode.Alpha2) && baseballBatUnlocked)
         {
             SwitchToWeapon(1);
         }
-        if (Input.GetKeyDown(KeyCode.Alpha3) && transform.childCount >= 3)
+        if (Input.GetKeyDown(KeyCode.Alpha3) && pistolUnlocked)
         {
             SwitchToWeapon(2);
         }
-        if (Input.GetKeyDown(KeyCode.Alpha4) && transform.childCount >= 4)
+        if (Input.GetKeyDown(KeyCode.Alpha4) && shotgunUnlocked)
         {
             SwitchToWeapon(3);
         }
-        if (Input.GetKeyDown(KeyCode.Alpha5) && transform.childCount >= 5)
+        if (Input.GetKeyDown(KeyCode.Alpha5) && bazookaUnlocked)
         {
             SwitchToWeapon(4);
         }
@@ -82,10 +74,18 @@ public class WeaponManager : MonoBehaviour
             if (handWeapon.activeSelf == true)
             {
                 GameObject projectileClone;
-                projectileClone = Instantiate(projectilePrefabHand, handFirePoint.transform.position, handFirePoint.transform.rotation) as GameObject;
+                projectileClone = Instantiate(projectilePrefabHand, firepointHand.transform.position, firepointHand.transform.rotation) as GameObject;
                 Destroy(projectileClone, 3f);
 
                 camShake.Shake(0.025f, 0.1f, Camera.main);
+            }
+            else if (baseballBat.activeSelf == true)
+            {
+                GameObject projectileClone;
+                projectileClone = Instantiate(projectilePrefabBaseballBat, firepointBaseballBat.transform.position, firepointBaseballBat.transform.rotation) as GameObject;
+                Destroy(projectileClone, 3f);
+
+                camShake.Shake(0.04f, 0.1f, Camera.main);
             }
             else if (pistol.activeSelf == true)
             {
@@ -114,11 +114,20 @@ public class WeaponManager : MonoBehaviour
         int i = 0;
         foreach (Transform weapon in transform)
         {
-            if (i == selectedWeapon) weapon.gameObject.SetActive(true);
+
+            if (i == selectedWeapon)
+            {
+                weapon.gameObject.SetActive(true);
+                iconContainer.sprite = iconsList[i];
+            }
             else
+            {
                 weapon.gameObject.SetActive(false);
+            }
+
             i++;
         }
+
     }
 
     public void SwitchToWeapon(int weaponSlot)
@@ -129,4 +138,49 @@ public class WeaponManager : MonoBehaviour
             SelectWeapon();
         }
     }
+
+    public void UnlockNewWeapon(string boolName)
+    {
+        if (boolName == "baseballBatUnlocked")
+        {
+            baseballBatUnlocked = true;
+        }
+        if (boolName == "pistolUnlocked")
+        {
+            pistolUnlocked = true;
+        }
+        if (boolName == "shotgunUnlocked")
+        {
+            shotgunUnlocked = true;
+        }
+    }
+
+    public void ScrollForth()
+    {
+        if (selectedWeapon >= transform.childCount - 1)
+        {
+            selectedWeapon = 0;
+        }
+        else
+        {
+            selectedWeapon++;
+        }
+        SelectWeapon();
+    }
+
+    public void ScrollBack()
+    {
+
+        if (selectedWeapon <= 0)
+        {
+            selectedWeapon = transform.childCount - 1;
+        }
+        else
+        {
+            selectedWeapon--;
+        }
+        SelectWeapon();
+    }
+
+
 }
